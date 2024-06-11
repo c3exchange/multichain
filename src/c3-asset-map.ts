3b898cd51f2393358f195542fa6072d49d7e5409
Joshua Averett <joshua.averett+github2@gmail.com>
2024-06-11 10:58:23 -0700

    Complete base entries for the asset map

--- src/c3-asset-map.ts
+++ src/c3-asset-map.ts
@@ -5,32 +5,23 @@ import { AssetName, ChainName } from './references'
 export const C3_ASSET_MAP: AssetMap = {
 	[AssetName.ALGO]: {
 		chains: {
-			
+			[ChainName.Algorand]: {
+				'': {
+					id: '0',
+					decimals: 6,
+				},
+			},
 		},
 	},
 	[AssetName.ARB]: {
 		chains: {
-			
-		},
-	},
-	[AssetName.AVAX]: {
-		chains: {
-			
-		},
-	},
-	[AssetName.BNB]: {
-		chains: {
-			
-		},
-	},
-	[AssetName.BTC]: {
-		chains: {
-			
-		},
-	},
-	[AssetName.ETH]: {
-		chains: {
-			[ChainName.Ethereum]: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '1221549217',
+					decimals: 6,
+				}
+			},
+			[ChainName.Arbitrum]: {
 				'': {
 					id: '0x0000000000000000000000000000000000000000',
 					decimals: 18,
@@ -38,13 +29,86 @@ export const C3_ASSET_MAP: AssetMap = {
 			},
 		},
 	},
+	[AssetName.AVAX]: {
+		chains: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '893309613',
+					decimals: 6,
+				},
+			},
+			[ChainName.Avalanche]: {
+				'': {
+					id: '0x0000000000000000000000000000000000000000',
+					decimals: 18,
+				},
+			},
+		},
+	},
+	[AssetName.BNB]: {
+		chains: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '891648844',
+					decimals: 6,
+				},
+			},
+			[ChainName.Binance]: {
+				'': {
+					id: '0x0000000000000000000000000000000000000000',
+					decimals: 18,
+				},
+			},
+		},
+	},
+	[AssetName.BTC]: {
+		chains: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '1058926737',
+					decimals: 6,
+				},
+			},
+		},
+	},
+	[AssetName.ETH]: {
+		chains: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '887406851',
+					decimals: 6,
+				},
+			},
+			[ChainName.Ethereum]: {
+				'': {
+					id: '0x0000000000000000000000000000000000000000',
+					decimals: 18,
+				},
+				'wrapped': {
+					id: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+					decimals: 18,
+				},
+			},
+		},
+	},
 	[AssetName.PYTH]: {
 		chains: {
-			
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '1684682524',
+					decimals: 6,
+				},
+			},
 		},
 	},
 	[AssetName.SOL]: {
 		chains: {
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '887648583',
+					decimals: 6,
+				},
+			},
 			[ChainName.Solana]: {
 				'': {
 					id: SystemProgram.programId.toBase58(),
@@ -55,12 +119,22 @@ export const C3_ASSET_MAP: AssetMap = {
 	},
 	[AssetName.USDC]: {
 		chains: {
-			
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '1007352535',
+					decimals: 6,
+				},
+			},
 		},
 	},
 	[AssetName.W]: {
 		chains: {
-			
+			[ChainName.Algorand]: {
+				'wormhole': {
+					id: '1703994770',
+					decimals: 6,
+				},
+			},
 		},
 	},
 }
